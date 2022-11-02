a0a2e34745e3fd67dc8c3c0a3504360d77e209a4
Mr. Fluffy <git@sixearsm>
2022-11-02 21:15:05 +0000

    more-unicode -> 0.0.17.2

--- flake.nix
+++ flake.nix
@@ -5,8 +5,8 @@
     nixpkgs.url       = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
     build-utils.url   = "github:sixears/flake-build-utils/r1.0.0.3";
 
-    has-callstack.url = "github:sixears/has-callstack/r1.0.1.3";
-    monaderror-io.url = "github:sixears/monaderror-io/r1.2.5.2";
+    has-callstack.url = "github:sixears/has-callstack/r1.0.1.4";
+    monaderror-io.url = "github:sixears/monaderror-io/r1.2.5.3";
     more-unicode.url  = "github:sixears/more-unicode/r0.0.17.2";
   };
 
