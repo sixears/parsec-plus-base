20dbc1ddf9dd4036f76149cc0f855eba24e32d1b
Mr. Fluffy <git@sixearsm>
2022-11-19 09:10:41 +0000

    add flake; use ghc-8.10.7 for tfmt

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Parsecable class, and utilities; base version without file parsing";
 
   inputs = {
-    nixpkgs.url       = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
+    nixpkgs.url       = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
     build-utils.url   = github:sixears/flake-build-utils/r1.0.0.13;
 
     has-callstack.url = github:sixears/has-callstack/r1.0.1.17;
@@ -17,5 +17,21 @@
         inherit has-callstack monaderror-io more-unicode;
       };
       ghc = p: p.ghc8107; # for tfmt
+      callPackage = { mkDerivation, lib, mapPkg, system
+                    , base, base-unicode-symbols, data-textual, deepseq, lens
+                    , mtl, parsec, text-printer
+                    }:
+        mkDerivation {
+          pname = "parsec-plus-base";
+          version = "1.0.5.21";
+          src = ./.;
+          libraryHaskellDepends = [
+            base base-unicode-symbols data-textual deepseq lens mtl parsec
+            text-printer
+          ] ++ mapPkg [ has-callstack monaderror-io more-unicode ];
+          testHaskellDepends = [ base ];
+          description = "Parsecable class, and utilities; base version without file parsing";
+          license = lib.licenses.mit;
+        };
     };
 }
