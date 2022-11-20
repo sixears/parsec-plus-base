72a2d6e20a12482af547675c58741be51836fef6
Mr. Fluffy <git@sixearsm>
2022-11-20 06:54:00 +0000

    exited->1.0.4.22;monaderror-io->1.2.5.19;tfmt->0.2.7.24;number->1.1.2.14;has-callstack->1.0.1.18;base0->0.0.4.11;more-unicode->0.0.17.12;quasiquoting->1.0.1.31

--- flake.nix
+++ flake.nix
@@ -5,9 +5,9 @@
     nixpkgs.url       = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
     build-utils.url   = github:sixears/flake-build-utils/r1.0.0.13;
 
-    has-callstack.url = github:sixears/has-callstack/r1.0.1.17;
-    monaderror-io.url = github:sixears/monaderror-io/r1.2.5.18;
-    more-unicode.url  = github:sixears/more-unicode/r0.0.17.11;
+    has-callstack.url = github:sixears/has-callstack/r1.0.1.18;
+    monaderror-io.url = github:sixears/monaderror-io/r1.2.5.19;
+    more-unicode.url  = github:sixears/more-unicode/r0.0.17.12;
   };
 
   outputs = { self, nixpkgs, build-utils
@@ -23,7 +23,7 @@
                     }:
         mkDerivation {
           pname = "parsec-plus-base";
-          version = "1.0.5.21";
+          version = "1.0.5.22";
           src = ./.;
           libraryHaskellDepends = [
             base base-unicode-symbols data-textual deepseq lens mtl parsec
